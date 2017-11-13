22f9f22c85ccc0614740deb53674865429ee37cb
Chad Lei <cblei@uci.edu>
2017-11-13 01:29:25 -0800

    2nd commit
    changes jquery

--- myScript.js
+++ myScript.js
@@ -1,19 +1,16 @@
-// function myFunction() {
-//    document.getElementById("demo").innerHTML = "Paragraph changed.";
-// }
-
-// function scrollToAbout() {
-//     scrollTop: $(".aboutpagecontainer").offset().top},'slow');
-// }
+$("clickedMain").click(function() {
+  	$('html,body').animate({
+    	scrollTop: $("#mainpage").offset().top},'slow');
+	}
+);
 
 $("#clickedAbout").click(function() {
     $('html, body').animate({
-        scrollTop: $("#aboutpage").offset().top
-    }, 2000);
-});
+        scrollTop: $("#aboutpage").offset().top}, 2000);
+	}	
+);
 
-$("clickedExperience").click(
-	function() {
+$("clickedExperience").click(function() {
   	$('html,body').animate({
     	scrollTop: $("#experiencepage").offset().top},'slow');
 	}
